24ce5ffc86a56bb18bd5561fa9156fc5c924b8ea
Christian <christianesl@gmail.com>
2020-08-19 22:25:16 -0500

    test from week2

--- Google Course/manage csv files.py
+++ Google Course/manage csv files.py
@@ -1,3 +1,12 @@
+#reading csv
+import csv
+f = open("csv_file.txt")
+cvf_f = csv.reader(f)
+for row in csv_f:
+	name, phone, role = row
+	print( "Name: {} Phone:{} Department:{}".format(name,phone, role))
+
+#writing to csv
 hosts = [["workstation.local", "192.168.25.56"], ["webserver.cloud", "10.2.5.6"]]
 with open('hosts.csv', 'w') as hosts_csv:
 	writer = csv.writer(hosts.csv)
---
+++ Google Course/manage dict csv.py
@@ -0,0 +1,16 @@
+#reading from the csv
+with open('software.csv') as software:
+	reader = csv.DictReader(software)
+	for row in reader:
+		print(("{} has {} users").format(row["name"], row["users"]))
+
+#writing to the csv
+users [ {"name":"name1", "username": "username1", "department": "deparment1"},
+		{"name":"name2", "username": "username2", "department": "deparment2"},
+		{"name":"name3", "username": "username3", "department": "deparment3"}]
+
+keys = ["name", "username", "deparment"]
+with open('by_department.csv', 'w') as by_department:
+	writer = csv.DictWriter(by_department, fieldnames=keys)
+	writer.writeheader()
+	writer.writerows(users)
