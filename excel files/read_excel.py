6183ad58a96018211c7d7d88ccad14915f01f058
Christian Saldana <christian.saldana@accenture.com>
2020-08-28 12:22:19 -0500

    completed the read excel file

--- excel files/read_excel.py
+++ excel files/read_excel.py
@@ -1,9 +1,19 @@
 import openpyxl
 
 book = openpyxl.load_workbook('data.xlsx')
+worksheet = book.worksheets[0]
 
-sheet = book.active
+main_list = []
 
-for row in sheet.iter_rows(min_row=1, min_col=1, max_row=10, max_col=5):
+for row in worksheet.iter_rows(min_row=2, min_col=1, max_row=worksheet.max_row, max_col=worksheet.max_column):
+	sub_lst = []
 	for cell in row:
-		print("value " + str(cell.value), end=" ")
+		if cell.value: 		
+			sub_lst.append(str(cell.value))
+	main_list.append(sub_lst)		
+		
+
+index = 0
+for lst in main_list:
+	print(lst)
+
